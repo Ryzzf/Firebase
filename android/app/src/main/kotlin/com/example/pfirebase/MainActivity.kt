0a6806aee59a86eba7f325bb1cc8b15f992f8121
unknown <ranggaeizy18@gmail.com>
2025-12-02 16:05:55 +0700

    firebase

---
+++ android/app/src/main/kotlin/com/example/pfirebase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pfirebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:pfirebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pfirebase/screen/Homepage.dart';
+import 'package:pfirebase/screen/login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+ );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(), 
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return CircularProgressIndicator();
+        }
+        if(snapshot.hasData){
+          return TampilanHome();
+        }
+        return TampilanLogin();
+      },
+      );
+  }
+}
